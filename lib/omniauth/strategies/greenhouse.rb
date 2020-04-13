85bd9d0fa526372ec6baad96006567cba408f917
Peter Boling <peter.boling@gmail.com>
2020-04-13 14:22:10 -0600

    Compatibility with omniauth-oauth2 >= 1.4
    Ref:
    * https://github.com/omniauth/omniauth-oauth2/commit/26152673224aca5c3e918bcc83075dbb0659717f
    * https://github.com/omniauth/omniauth-oauth2/pull/70

--- lib/omniauth/strategies/greenhouse.rb
+++ lib/omniauth/strategies/greenhouse.rb
@@ -4,5 +4,13 @@ module OmniAuth
       option :name, 'greenhouse'
       option :client_options, { :site => "https://api.greenhouse.io" }
     end
+
+    # For compatibility with omniauth-oauth2 >= 1.4
+    # Ref:
+    # https://github.com/omniauth/omniauth-oauth2/commit/26152673224aca5c3e918bcc83075dbb0659717f
+    # https://github.com/omniauth/omniauth-oauth2/pull/70
+    def callback_url
+      full_host + script_name + callback_path
+    end
   end
 end
